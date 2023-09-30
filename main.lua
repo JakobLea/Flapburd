6ae32e342eb1d70e7efce9dcbc94cc98c3a8cab0
JakobLea <jakob.lea@outlook.com>
2023-10-01 01:08:41 +0200

    bird4

--- main.lua
+++ main.lua
@@ -1,24 +1,40 @@
 push = require 'push'
 
+Class = require 'class'
+
+require 'Bird'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
-VIRTUAL_WINDOW = 512 
+VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
 local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    love.window.setTitle('BORD')
+    love.window.setTitle('Im Birdman, thats who I am. In Paris. Brr brr')
 
-    push:setupScreen(VIRTUAL_WINDOW, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
-        resizable = true,
+        resizable = true
     })
+
+    love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -26,15 +42,40 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+
     if key == 'escape' then
         love.event.quit()
     end
 end
 
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) 
+        % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) 
+        % VIRTUAL_WIDTH
+
+    bird:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
 function love.draw()
     push:start()
-    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(background, -backgroundScroll, 0)
 
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    bird:render()
+
     push:finish()
 end
