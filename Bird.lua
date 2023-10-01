519d9e85f510700ce5488c1d339f5e70cdf4f364
JakobLea <jakob.lea@outlook.com>
2023-10-01 16:04:17 +0200

    states + hitboxfix + score

--- Bird.lua
+++ Bird.lua
@@ -14,8 +14,8 @@ function Bird:init()
 end
 
 function Bird:collides(pipe)
-    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
-        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+    if (self.x - 5) + (self.width - 5) >= pipe.x and self.x + 7 <= pipe.x + PIPE_WIDTH then
+        if (self.y - 5) + (self.height - 5) >= pipe.y and self.y + 7 <= pipe.y + PIPE_HEIGHT then
             return true
         end
     end
@@ -27,7 +27,7 @@ function Bird:update(dt)
     self.dy = self.dy + GRAVITY * dt
 
     if love.keyboard.wasPressed('space') then
-        self.dy = -2
+        self.dy = -1.5
     end
 
     self.y = self.y + self.dy
